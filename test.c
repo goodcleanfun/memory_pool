3e563ad3df2f4ad61616f9b4be9720a883599a4f
Al <albarrentine@gmail.com>
2024-11-11 00:25:42 -0500

    [fix] stdint for tests

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -59,7 +60,7 @@ TEST test_memory_pool(void) {
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
 
-int32_t main(int32_t argc, char **argv) {
+int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
 
     RUN_TEST(test_memory_pool);
