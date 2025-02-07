eba2b03f3ae8296b9bbb67c290620dabe38a2b1f
Al <albarrentine@gmail.com>
2025-02-07 01:48:08 -0500

    [test/thread_safe] adding thread-safe memory pool tests

--- test.c
+++ test.c
@@ -18,6 +18,116 @@ typedef struct bintree_node {
 #undef MEMORY_POOL_NAME
 #undef MEMORY_POOL_TYPE
 
+#define MEMORY_POOL_NAME bintree_node_concurrent_memory_pool
+#define MEMORY_POOL_TYPE bintree_node_t
+#define MEMORY_POOL_THREAD_SAFE
+#include "memory_pool.h"
+#undef MEMORY_POOL_NAME
+#undef MEMORY_POOL_TYPE
+#undef MEMORY_POOL_THREAD_SAFE
+
+typedef struct {
+    bintree_node_concurrent_memory_pool *pool;
+    bintree_node_t **nodes;
+} bintree_concurrent_memory_pool_test_arg_t;
+
+int bintree_memory_pool_create_thread(void *arg) {
+    bintree_concurrent_memory_pool_test_arg_t *input = (bintree_concurrent_memory_pool_test_arg_t *)arg;
+    bintree_node_concurrent_memory_pool *pool = input->pool;
+    bintree_node_t **nodes = input->nodes;
+
+    bintree_node_t *node;
+
+    for (size_t i = 0; i < pool->block_size; i++) {
+        node = bintree_node_concurrent_memory_pool_get(pool);
+        if (node == NULL) {
+            return 1;
+        }
+        nodes[i] = node;
+    }
+
+    for (size_t i = 0; i < pool->block_size; i++) {
+        node = nodes[i];
+        if (!bintree_node_concurrent_memory_pool_release(pool, nodes[i])) {
+            return 1;
+        }
+        nodes[i] = NULL;
+    }
+    return 0;
+}
+
+#define NUM_THREADS 8
+
+TEST test_concurrent_memory_pool(void)  {
+    bintree_node_concurrent_memory_pool *pool = bintree_node_concurrent_memory_pool_new();
+    ASSERT(pool != NULL);
+    ASSERT_EQ(pool->num_blocks, 1);
+
+    size_t max_nodes = pool->block_size * NUM_THREADS;
+    bintree_node_t **nodes = malloc(max_nodes * sizeof(bintree_node_t *));
+
+    thrd_t create_threads[NUM_THREADS];
+    bintree_concurrent_memory_pool_test_arg_t args[NUM_THREADS];
+
+    for (size_t i = 0; i < NUM_THREADS; i++) {
+        args[i] = (bintree_concurrent_memory_pool_test_arg_t){
+            .pool = pool,
+            .nodes = nodes + (i * pool->block_size)
+        };
+        thrd_create(&create_threads[i], bintree_memory_pool_create_thread, &args[i]);
+    }
+    for (size_t i = 0; i < NUM_THREADS; i++) {
+        thrd_join(create_threads[i], NULL);
+    }
+
+    size_t free_list_size = 0;
+    bintree_node_concurrent_memory_pool_free_list_t free_list = atomic_load(&pool->free_list);
+    bintree_node_concurrent_memory_pool_item_t *item = free_list.node;
+
+    bintree_node_concurrent_memory_pool_item_t *prev_item = NULL;
+
+    while (item != NULL) {
+        for (size_t i = 0; i < free_list_size; i++) {
+            if (nodes[i] == (bintree_node_t *)item) {
+                fprintf(stderr, "duplicate node: %p, prev_item=%p, prev_i=%zu, free_list_size=%zu\n", item, prev_item, i, free_list_size);
+                FAIL();
+            }
+        }
+        nodes[free_list_size++] = (bintree_node_t *)item;
+        ASSERT(free_list_size <= NUM_THREADS * pool->block_size);
+        prev_item = item;
+        item = item->next;
+    }
+    free(nodes);
+
+    bintree_node_concurrent_memory_pool_destroy(pool);
+    PASS();
+}
+
+int bintree_memory_pool_thread(void *arg) {
+    bintree_node_concurrent_memory_pool *pool = (bintree_node_concurrent_memory_pool *)arg;
+
+    bintree_node_t **nodes = malloc(pool->block_size * sizeof(bintree_node_t *));
+    bintree_node_t *node;
+
+    for (size_t i = 0; i < pool->block_size; i++) {
+        node = bintree_node_concurrent_memory_pool_get(pool);
+        if (node == NULL) {
+            return 1;
+        }
+        nodes[i] = node;
+    }
+    for (size_t i = 0; i < pool->block_size; i++) {
+        node = nodes[i];
+        if (!bintree_node_concurrent_memory_pool_release(pool, node)) {
+            return 1;
+        }
+    }
+    free(nodes);
+    return 0;
+}
+
+
 TEST test_memory_pool(void) {
     bintree_node_memory_pool *pool = bintree_node_memory_pool_new();
     ASSERT(pool != NULL);
@@ -56,7 +166,6 @@ TEST test_memory_pool(void) {
 }
 
 
-
 /* Add definitions that need to be in the test runner's main file. */
 GREATEST_MAIN_DEFS();
 
@@ -64,6 +173,7 @@ int main(int argc, char **argv) {
     GREATEST_MAIN_BEGIN();      /* command-line options, initialization. */
 
     RUN_TEST(test_memory_pool);
+    RUN_TEST(test_concurrent_memory_pool);
 
     GREATEST_MAIN_END();        /* display results */
 }
